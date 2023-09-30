95f3ba32782b167169c1cfc6379f06d0bdb61db0
Scott Moon <47267960+interactord@users.noreply.github.com>
2023-09-30 21:21:16 +0900

    데모 모듈 삭제

--- Project/Feature/Demo/Sources/Demo/Source/Base/DemoSideEffectGroup.swift
+++
@@ -1,5 +0,0 @@
-import Architecture
-import Foundation
-import LinkNavigator
-
-public protocol DemoSideEffectGroup: DependencyType { }
--- Project/Feature/Demo/Sources/Demo/Source/Page/Page4/Page4Store.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//
-//
-//  Created by 승찬 on 2023/09/04.
-//
-
-import Foundation
